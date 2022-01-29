234e951f3dfdcf9db23f53a9b8c4e2ff3a5d8508
Cinthia Gisela Camacho <gis25cam@gmail.com>
2022-01-29 10:54:27 -0600

    Manejo de formulario y header

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Exercise from './components/Exercise';
+import Layout from './components/Layout'
 // FUNCION
 function App() {
   return (
     //Establece configuración de ruteo 
     <BrowserRouter>
-    
+    {/*Establece la lista de rutas */}
     <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/ejercicio" element={ <Exercise />} />
+
+      <Route path="/" element={ <Layout />} >
+
+        <Route path="/" element={ <Home />} />
+        <Route path="/ejercicio" element={ <Exercise />} />
+
+      </Route>
+
     </Routes>
     </BrowserRouter>
   );
